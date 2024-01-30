b593845b7728df77e09f679c2050928631e55ad5
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-30 21:50:56 +0100

    seed the data base with movies

--- src/main/java/com/mohammedfares/movies_api/MoviesApiApplication.java
+++ src/main/java/com/mohammedfares/movies_api/MoviesApiApplication.java
@@ -1,13 +1,32 @@
 package com.mohammedfares.movies_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mohammedfares.movies_api.models.Movie;
+import com.mohammedfares.movies_api.servises.MovieServiceImpl;
 
 @SpringBootApplication
 public class MoviesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApiApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner comandlineRunner(MovieServiceImpl movieServiceImpl) {
+		return (e)->{
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+			movieServiceImpl.insertMovie(new Movie("Elf", "Raised as an oversized elf, Buddy travels from the North Pole to New York City to meet his biological father, Walter Hobbs, who doesn't know he exists and is in desperate need of some Christmas spirit.", 9.99f, "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/53057_500x749.jpg?v=1604088239", "2003"));
+		};
+		
 	}
 
 }
