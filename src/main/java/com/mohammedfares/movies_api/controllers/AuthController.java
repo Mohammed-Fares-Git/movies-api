ede132707d8ae9dedb759368bcdb4d3360bd0242
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-31 23:04:54 +0100

    create auth end points

--- src/main/java/com/mohammedfares/movies_api/controllers/AuthController.java
+++ src/main/java/com/mohammedfares/movies_api/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.mohammedfares.movies_api.controllers;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mohammedfares.movies_api.models.AuthRequest;
 import com.mohammedfares.movies_api.models.AuthResponse;
+import com.mohammedfares.movies_api.models.RegisterRequest;
 import com.mohammedfares.movies_api.servises.MovieServiceImpl;
 import com.mohammedfares.movies_api.servises.UserServiceImpl;
 
@@ -16,17 +18,17 @@ import com.mohammedfares.movies_api.servises.UserServiceImpl;
 public class AuthController {
 	
 	private final UserServiceImpl userServiceImpl;
-	public AuthController(UserServiceImpl userServiceImpl) {
+	public AuthController(@Lazy UserServiceImpl userServiceImpl) {
 		this.userServiceImpl = userServiceImpl;
 	}
 
 	@PostMapping("/register") 
-	public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest authRequest){
-		return null;
+	public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest){
+		return ResponseEntity.ok(this.userServiceImpl.register(registerRequest));
 	}
 	
 	@PostMapping("/authenticate") 
 	public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest){
-		return null;
+		return ResponseEntity.ok(this.userServiceImpl.authenticate(authRequest));
 	}
 }
