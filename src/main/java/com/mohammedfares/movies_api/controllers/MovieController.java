cee27e8f8eddc9c8ca2c514d3a18f874b456cfcb
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-02 12:45:01 +0100

    hundle cors

--- src/main/java/com/mohammedfares/movies_api/controllers/MovieController.java
+++ src/main/java/com/mohammedfares/movies_api/controllers/MovieController.java
@@ -3,13 +3,14 @@ package com.mohammedfares.movies_api.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mohammedfares.movies_api.models.Movie;
 import com.mohammedfares.movies_api.servises.MovieServiceImpl;
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
